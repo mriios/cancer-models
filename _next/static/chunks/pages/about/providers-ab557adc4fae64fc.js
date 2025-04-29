1f7d65897a9f90ee184bd76d88240fa0490862a0
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2025-04-29 15:00:10 +0100

    "Deploy"

---
+++ _next/static/chunks/pages/about/providers-ab557adc4fae64fc.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[722],{1199:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/about/providers",function(){return u(8987)}])}},function(n){n.O(0,[675,553,987,774,888,179],function(){return n(n.s=1199)}),_N_E=n.O()}]);
---
+++ _next/static/g698LSgwwopGJmnGI0VKL/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fabout\u002Fproviders","\u002Fabout\u002Fproviders\u002F[id]","\u002Fdata\u002Fmodels\u002F[providerId]\u002F[modelId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
