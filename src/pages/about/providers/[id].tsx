300b455a5d34c834d6b87c68a1315da853edaeda
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2025-05-09 15:32:00 +0100

    fix provider logo path

--- src/pages/about/providers/[id].tsx
+++ src/pages/about/providers/[id].tsx
@@ -34,7 +34,7 @@ const Provider: NextPage<ProviderProps> = ({ providerData }: ProviderProps) => {
 						<div className="col-12 col-md-3">
 							{providerData.logo && (
 								<Image
-									src={`./${providerData.logo}`}
+									src={`${providerData.logo}`}
 									alt={`${providerData.name} logo`}
 									width={3500}
 									height={3500}
