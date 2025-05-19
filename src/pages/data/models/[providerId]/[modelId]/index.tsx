27287b91704b4c3614a243141ae196c9d6260162
Marcelo Rios Almanza <marcelo10rios@hotmail.com>
2025-05-19 13:58:30 +0100

    move modelid page inside dynamic folder

--- src/pages/data/models/[providerId]/[modelId]/index.tsx
+++ src/pages/data/models/[providerId]/[modelId]/index.tsx
@@ -1,8 +1,16 @@
 import "driver.js/dist/driver.css";
+import dynamic from "next/dynamic";
 import { useRouter } from "next/router";
 import { useQuery } from "react-query";
-import { getAllModelData } from "../../../../apis/ModelDetails.api";
-import ModelPage from "../../../../components/ModelPage/ModelPage";
+import { getAllModelData } from "../../../../../apis/ModelDetails.api";
+import ModelPage from "../../../../../components/ModelPage/ModelPage";
+
+const DynamicLoader = dynamic(
+	() => import("../../../../../components/Loader/Loader"),
+	{
+		ssr: false
+	}
+);
 
 const ModelDetails = () => {
 	const router = useRouter();
@@ -14,8 +22,14 @@ const ModelDetails = () => {
 		{ enabled: !!modelId && !!providerId }
 	);
 
-	if (isLoading) return <div>Loading...</div>;
-	if (isError || !data) return <div>Sorry, there has been an error</div>;
+	if (isLoading)
+		return (
+			<div style={{ height: "300px" }} className="w-100">
+				<DynamicLoader />
+			</div>
+		);
+	if (isError || !data)
+		return <div className="text-center">Sorry, there has been an error</div>;
 
 	return <ModelPage {...data} />;
 };
